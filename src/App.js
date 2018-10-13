a12b4e80cda6aeeb2d563d87e7327198a0fa2fa5
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-13 17:00:02 +0300

    Add feature-based folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import React, { Component } from 'react';
-import MapContainer from './MapContainer.js';
+import MapContainer from './MapContainer/MapContainer.js';
 
 class App extends Component {
   render() {    
