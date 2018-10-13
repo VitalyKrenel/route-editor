f0e507ae080d09ae2c15ac178f407100ac1b9f19
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-13 20:19:47 +0300

    Implement PointInput as the controlled component
    @see https://reactjs.org/docs/forms.html#controlled-components

---
+++ src/PointInput/PointInput.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class PointInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    const { value } = this.state;
+    this.props.onSubmit(value);
+    this.setState({ value: '' });
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input className="App-Input" type="text" value={this.state.value} onChange={this.handleChange} />
+      </form>
+    );
+  }
+}
