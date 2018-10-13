a24ae5ea6a8e5b590dc82d1110c44e444aefc461
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-13 13:21:58 +0300

    Design a basic app structure in React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="App">
+        <div className="App-Dashboard">
+          <PointInput />
+          <PointList />
+        </div> 
+        <Map />
+      </main>
     );
   }
+}
+
+function Map(props) {
+  return (
+    <div className="App-Map" id="map"></div>
+  );
+}
+
+function PointInput (props) {
+  return (
+    <input className="App-Input" type="text"/>
+  );
+}
+
+function PointList(props) {
+  return (
+    <ul className="App-List"></ul>
+  );
 }
 
 export default App;
