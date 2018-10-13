cd82d09566547c9443a0f41be7fb722501772326
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-13 22:18:04 +0300

    Add simplified PointList component with basic styles
    It's a little confusing where in feature-based project structure should
    be stored general purpose styles like ".Item" class.
    
    It has no sense to import PointList.css only to use ".Item" class.
    Perhaps on the top level (src/) has to be a folder with general css
    files, where each file dedicated to a specific css Block (BEM).

---
+++ src/PointList/PointList.js
@@ -0,0 +1,27 @@
+import './PointList.css';
+
+import React from 'react';
+
+export default function PointList(props) {
+  const { locations } = props;
+
+  const listItems =
+    locations.map((location) => (<PointListItem location={location} key={location}/>));
+
+  return (
+    <ul className="PointList">
+      {listItems}
+    </ul>
+  );
+}
+
+export function PointListItem(props) {
+  const { location } = props;
+
+  return (
+    <li className="PointList-Item Item">
+      <div className="Item-Location">{location}</div>
+      <button className="Item-Button Item-Button_action_remove">×</button>
+    </li>
+  ); 
+}
