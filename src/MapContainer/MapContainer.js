e6e2227731ab3ad1587b495db35b27ae97a6fce6
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-14 21:31:39 +0300

    Render multiRoute on map when locations get updated
    A debatable decision was made:
    
    - I implemented createRoute function with multiRoute instead of simple
    route class (or Placemarks + Polyline) because the former class allows
    more smooth (though more complex) route path rendering;
    
    I'm not sure whether this option violates the assignment statement,
    perhaps I will introduce another branch with more assignment-precise
    implementation.

--- src/MapContainer/MapContainer.js
+++ src/MapContainer/MapContainer.js
@@ -1,10 +1,10 @@
 import './MapContainer.css';
 
 import React, { Component } from 'react';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { YMaps, Map } from 'react-yandex-maps';
 
 const apikey = '262287d2-a40d-4b35-b808-7d4231cb5915';
-// const modules = ['geocode'];
+const modules = ['route'];
 const mapState = { center: [55.75, 37.57], zoom: 9 };
 
 export const generateGeometry = () => {
@@ -30,6 +30,20 @@ export default class MapContainer extends Component {
     this.ymaps = ymaps;
     this.setState({ mapIsLoaded: true });
   }
+
+  createRoute(locations) {
+    const points = locations.map((location) => location.value);   
+
+    const routePromise = this.ymaps.route(points, { 
+      multiRoute: true 
+    });
+    routePromise.then(
+      (multiRoute) => {
+        this.map.geoObjects.removeAll();
+        this.map.geoObjects.add(multiRoute);
+      },
+      (err) => console.error(err),
+    );
   }
 
   render() {
@@ -43,27 +57,22 @@ export default class MapContainer extends Component {
       mapContainer.className += ' MapContainer_status_loading';
     }
 
-    const { placemarks } = this.props; 
+    const { locations } = this.props;
 
-    const placemarkComponents = placemarks.map((location) => (
-      <Placemark 
-        geometry={location.geometry}
-        key={location.id} 
-        options={{ preset: 'islands#blueCircleDotIconWithCaption'}}
-      />
-    ));
+    if (this.ymaps && locations.length !== 0) {
+      this.createRoute(locations);
+    }
 
     return (
       <div { ...mapContainer }>
         <YMaps query={{apikey}}>
-          <Map 
-            className="Map"  
-            // modules={modules}  
+          <Map
+            className="Map"
+            modules={modules}  
             defaultState={mapState} 
             onLoad={this.handleLoad}
             instanceRef={ref => (this.map = ref)}
           >
-            {placemarkComponents}
           </Map>
         </YMaps>
       </div>
