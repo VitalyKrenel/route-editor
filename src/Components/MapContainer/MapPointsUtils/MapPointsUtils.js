d4b9a7019d97118b5d4edb9fedb398d4421875c6
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-23 01:07:40 +0300

    Update diffPoints function to perform comparisons with coords

--- src/Components/MapContainer/MapPointsUtils/MapPointsUtils.js
+++ src/Components/MapContainer/MapPointsUtils/MapPointsUtils.js
@@ -1,6 +1,14 @@
 /**
  * Compare locationsPoints and wayPoints. If any locPoint does not
- * match a wayPoint, return it with updated data (adress).
+ * match a wayPoint, return an object representing locPoint `index` in locations array, and wayPoint `coords`, `address` and `request`.
+ * 
+ * **Note**: `address` property will be avaliable only if 
+ * the `reverseGeocoding` option for multiRouter is set to true, otherwise it's
+ * undefined.
+ * 
+ * **Note**: `request` is either a requesting address or requesting coordinates.
+ * 
+ * **Note**: Way point's `address` and `request` properties most of the time do not match because address is formatted by Yandex.
  * 
  * @param {Array} locationPoints 
  * @param {Array} routeWayPoints
@@ -13,6 +21,15 @@ export function diffPoints(locationPoints, routeWayPoints) {
 
   locationPoints.forEach((locPoint, index) => { 
     const foundIndex = wayPoints.findIndex((wayPoint) => {
+      // Match loc and way point with coords if defined or fallback to
+      // address comparison  
+      if (locPoint.coords.length !== 0) {
+        const wayPointCoords = wayPoint.geometry.getCoordinates();
+        return (
+          locPoint.coords[0] === wayPointCoords[0] &&
+          locPoint.coords[1] === wayPointCoords[1]
+        );
+      }
       return locPoint.value === wayPoint.properties.get('request');
     });
     
