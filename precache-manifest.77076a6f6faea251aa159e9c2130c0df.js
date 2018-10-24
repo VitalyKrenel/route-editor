d3d1777cf0335a4bdc0c1749e24ea8394180ab42
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-24 21:45:45 +0300

    Updates

---
+++ precache-manifest.77076a6f6faea251aa159e9c2130c0df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5796f7bca18e7f0bf9193cce371f41e2",
+    "url": "/route-editor/static/media/drag-and-drop.5796f7bc.svg"
+  },
+  {
+    "revision": "6026b84b725f2de728a8e3ae66ed9965",
+    "url": "/route-editor/static/media/cancel.6026b84b.svg"
+  },
+  {
+    "revision": "79bd776df1672a192416",
+    "url": "/route-editor/static/js/runtime~main.79bd776d.js"
+  },
+  {
+    "revision": "0b98082ade25fe335bee",
+    "url": "/route-editor/static/js/main.0b98082a.chunk.js"
+  },
+  {
+    "revision": "e6f659ff506cd3a88b6f",
+    "url": "/route-editor/static/js/1.e6f659ff.chunk.js"
+  },
+  {
+    "revision": "0b98082ade25fe335bee",
+    "url": "/route-editor/static/css/main.28969dd1.chunk.css"
+  },
+  {
+    "revision": "f83706f3a885ef6a3745c5efdbf0ecd8",
+    "url": "/route-editor/index.html"
+  }
+];
