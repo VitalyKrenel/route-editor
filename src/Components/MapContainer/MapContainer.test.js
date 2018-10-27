a74b28d3ce7527b6fd7161d4f6ceeccd57306994
Vitaly Krenel <vitaly.post@yahoo.com>
2018-10-27 22:29:08 +0300

    Update MapContainer tests

--- src/Components/MapContainer/MapContainer.test.js
+++ src/Components/MapContainer/MapContainer.test.js
@@ -1,38 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import sinon from 'sinon';
-
+import { YMaps } from 'react-yandex-maps';
 import MapContainer from './MapContainer.js';
-import { diffPoints } from 'Utils/MapPoints.js';
 import { makeLocationPointFactory } from 'LocationPoint/LocationPoint.js';
-
-jest.mock('Utils/MapPoints.js');
 
 const createLocationPoint = makeLocationPointFactory();
 const sandbox = sinon.createSandbox();
 
-const getFakeInitialRoute = () => ({
-  model: {
-    events: {
-      once: sinon.fake(),
-      add: sinon.fake(),
-    },
-    setReferencePoints: sinon.fake(),
-  },
-  getWayPoints: sinon.fake(() => ({
-    get() {
-      return { 
-        geometry: { getCoordinates: sinon.fake() }
-      };
-    },
-    getLength: sinon.fake(),
-    toArray: sinon.fake(() => []),
-  })),
-});
-
 describe('<MapContainer />', () => {  
   let wrapper;
   let component;
+  let handleBoundsChange = sinon.fake();
   
   const locations = [
     createLocationPoint('Москва'),
@@ -41,53 +20,35 @@ describe('<MapContainer />', () => {
   ];
 
   beforeEach(() => {
-    wrapper = shallow(<MapContainer locations={locations} />);
+    sandbox.stub(MapContainer.prototype, 'getPlacemark');
+    wrapper = shallow(
+      <MapContainer
+        locations={locations}
+        onBoundsChange={handleBoundsChange}
+      />
+    );
     component = wrapper.instance();
-
-    sandbox.stub(component, 'updateRoute');
-    component.ymaps = true;
-    component.initialRoute = getFakeInitialRoute();
   });
 
   afterEach(() => {
     sandbox.restore();
-    delete component.ymaps;
-    delete component.initialRoute;
+    handleBoundsChange.resetHistory();
+  })
+
+  it('renders without crashes', () => {
+    expect(wrapper).toHaveLength(1);
   });
 
-  it('calls updateRoute when recives props', () => {
-    const locations = [createLocationPoint('Франция')];
-
-    sandbox.assert.notCalled(component.updateRoute);
-    wrapper.setProps({ locations });
-
-    sandbox.assert.calledOnce(component.updateRoute);
+  it('calls getPlacemark for each location point', () => {
+    expect(component.getPlacemark.callCount).toBe(locations.length);
   });
 
-  it('calls updateRoute with correct arguments', () => {
-    const locations = [createLocationPoint('Франция')];
-    
-    wrapper.setProps({ locations });
-
-    sandbox.assert.calledWith(component.updateRoute, locations);
-  });
-
-  it('calls updateMapCenter after a new locationPoint was added', () => {
-    const updatedLocations = locations.concat([
-      createLocationPoint('Москва'),
-    ]);
-    const { initialRoute } = component;
-    sandbox.stub(component, 'updateMapCenter');
-
-    wrapper.setProps({ locations: updatedLocations });
-
-    sandbox.assert.calledOnce(
-      initialRoute.model.events.once
-    );
-
-    const shouldCallUpdateMap = initialRoute.model.events.once.args[0][1];
-    shouldCallUpdateMap();
-  
-    sandbox.assert.calledOnce(component.updateMapCenter);
-  });
+  /**
+   * 
+   * - Adds a new PlaceMark on Map when new location point is added
+   * - Updates Polyline geometry when new point is added
+   * - 
+   * 
+   * 
+   */
 });
